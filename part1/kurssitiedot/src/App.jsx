3cbba1244fe1145fe2bb9be488ada2cdc63e5060
vaddee <valtteri.laakso@hotmail.fi>
2024-06-10 15:36:35 +0300

    kurssitiedot teht valmis
    1.1 - 1.2 teht valmis

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return (
+      <div>
+          <h1>
+              {props.course.name}
+          </h1>
+      </div>)
 
-function App() {
-  const [count, setCount] = useState(0)
+}
+
+
+const Content = (props) => {
+  return(
+      <div>
+          <Part parts={props.parts[0]} />
+          <Part parts={props.parts[1]} />
+          <Part parts={props.parts[2]} />
+      </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+      <div>
+          <p>Number of excercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+      <div>
+          <p>{props.parts.name} {props.parts.exercises}</p>
+      </div>
+  )
+
+}
+
+const App = () => {
+  const course = {
+  name: 'Half Stack application development',
+
+      parts: [
+          {
+              name: 'Fundamentals of React',
+              exercises: 10
+          },
+          {
+              name: 'Using props to pass data',
+              exercises: 7
+          },
+          {
+              name: 'State of a component',
+              exercises: 14
+          }
+      ]
+  }
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <Header course={course} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
