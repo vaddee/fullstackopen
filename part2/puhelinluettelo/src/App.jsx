1ccfd08953f0d442411daa98d7f1d0127c671cd9
vaddee <valtteri.laakso@hotmail.fi>
2024-06-27 14:43:35 +0300

    puhelinluettelo step5 done

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -14,7 +21,7 @@ const App = () => {
       number: newNumber
     }
 
-    if (persons.some(person => person.name === newName)) {
+    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`)
     } else {
       setPersons(persons.concat(personObject))
@@ -31,27 +38,30 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person => 
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map((person, index) => 
-          <li key={index}>{person.name} {person.number}</li>
-        )}
-      </ul>
-      <div>debug: {newName} {newNumber}</div>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm 
+        onSubmit={addPerson} 
+        newName={newName} 
+        handleNameChange={handleNameChange}
+        newNumber={newNumber} 
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
     </div>
   )
 }
---
+++ part2/puhelinluettelo/src/components/Persons.jsx
@@ -0,0 +1,12 @@
+const Persons = ({ persons }) => {
+    return (
+      <ul>
+        {persons.map((person, index) => 
+          <li key={index}>{person.name} {person.number}</li>
+        )}
+      </ul>
+    )
+  }
+  
+  export default Persons
+  
